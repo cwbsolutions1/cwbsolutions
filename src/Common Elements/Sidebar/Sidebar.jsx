ef2edb083a2cec6603f9cd2da0895b5fb2ce46ed
Thamaru <thamarujalthotage1@gmail.com>
2024-10-23 10:53:07 +0530

    Sidebar dropdown items will be activated on mouse hover

--- src/Common Elements/Sidebar/Sidebar.jsx
+++ src/Common Elements/Sidebar/Sidebar.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Dropdown, Button, Offcanvas } from 'react-bootstrap';
-import './Sidebar.css'
+import './Sidebar.css';
 
 const Sidebar = () => {
   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 992);
-
+  
   // Check the screen size on resize
   useEffect(() => {
     const handleResize = () => {
@@ -16,180 +16,189 @@ const Sidebar = () => {
   }, []);
 
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  // Extracted dropdowns to a function to avoid redundancy
+  // State for dropdown visibility
+  const [openDropdown, setOpenDropdown] = useState(null);
+
   const renderDropdowns = () => (
     <>
-        <Dropdown  align="left">
+      <Dropdown 
+        align="left" 
+        onMouseEnter={() => setOpenDropdown('equipmentFor')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'equipmentFor'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Equipment For
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href="">Condominiums</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href="">Petrol Stations</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
 
-            <Dropdown.Toggle className="custom-sidebar-button" >
-                Equipment For
-            </Dropdown.Toggle>
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('productsAndServices')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'productsAndServices'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Our Products and Services
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Car washing and car wash bays equipment</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
 
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href="">Condominiums</Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href="">Petrol Stations</Dropdown.Item>
-            </Dropdown.Menu>
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('barrierAccess')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'barrierAccess'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Barrier and Access Control
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Vehicle License Plate Recognizer</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Car & Access Barrier Systems</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Parking and Access Control</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
 
-        </Dropdown>
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('keyStorage')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'keyStorage'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Key and Storage Management System
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>IQ Key Safe Series</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>IQ Wall Cabinet Series</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>IQ Box Cabinet Series</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>    
 
-        <Dropdown>
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('roadSafety')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'roadSafety'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Road Safety Products
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Road Humps</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>       
 
-            <Dropdown.Toggle className="custom-sidebar-button">
-                Our Products and Services
-            </Dropdown.Toggle>
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('roadAccessories')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'roadAccessories'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Road Accessories
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Solar Powered Studs</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>    
 
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Car washing and car wash bays equipment </Dropdown.Item>
-            </Dropdown.Menu>
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('perimeterFencing')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'perimeterFencing'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Perimeter Fencing & Gates
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>BRC Weld Fence</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>  
 
-        </Dropdown>
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('vehicularCare')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'vehicularCare'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Vehicular Self-Service Care Products
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Automatic Car Washes</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Vending Machines</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Powerful Car Vacuums</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Air Machines</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Hot Air Dryer</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Water Dispensers</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>  
 
-        <Dropdown>
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('electronicEngineering')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'electronicEngineering'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Electronic & Engineering Products & Services
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Coin & Cash Acceptors</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>PCB Boards</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Coin Operated Showers</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>  
 
-            <Dropdown.Toggle className="custom-sidebar-button">
-                Barrier and Access Control
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Vehicle Liecese Plate Recorgnizer </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Car & AccessBarrier Systems </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Parking and Access Control</Dropdown.Item>
-            </Dropdown.Menu>
-
-        </Dropdown>
-
-        <Dropdown>
-
-            <Dropdown.Toggle className="custom-sidebar-button">
-                Key and Storage Management system
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> IQ Key Safe series </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> IQ Wall Cabinet series </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> IQ BoxCabinet series</Dropdown.Item>
-            </Dropdown.Menu>
-
-        </Dropdown>    
-
-        <Dropdown>
-
-            <Dropdown.Toggle className="custom-sidebar-button">
-                Road Safety Products
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Road Humps </Dropdown.Item>
-            </Dropdown.Menu>
-
-        </Dropdown>       
-
-        <Dropdown>
-
-            <Dropdown.Toggle className="custom-sidebar-button">
-                Road Accessories
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Solar Powered Studs </Dropdown.Item>
-            </Dropdown.Menu>
-
-        </Dropdown>    
-
-        <Dropdown>
-
-            <Dropdown.Toggle className="custom-sidebar-button">
-                Perimeter Fencing & Gates
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> BRC WELDFENCE </Dropdown.Item>
-            </Dropdown.Menu>
-
-        </Dropdown>  
-
-        <Dropdown>
-
-            <Dropdown.Toggle className="custom-sidebar-button">
-                    Vehicular Self-Service Care products
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Automatic Car Washes </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Vending Machines </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Vending Machines </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Powerful Car Vacuums </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Air Machines </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Hot Air Dryer </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Water Dispensers </Dropdown.Item>
-            </Dropdown.Menu>
-
-        </Dropdown>  
-
-        <Dropdown>
-
-            <Dropdown.Toggle className="custom-sidebar-button">
-                Electronic & Engineering Products & Services
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Coin & Cash Acceptors </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> PCB Boards </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Coin Operated Showers </Dropdown.Item>
-            </Dropdown.Menu>
-
-        </Dropdown>  
-
-        <Dropdown>
-
-            <Dropdown.Toggle className="custom-sidebar-button">
-                Parks & Outdoor Furniture
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu id='sidebar-dropdown-menu'>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Electric BBQ Pits </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Outdoor Furniture </Dropdown.Item>
-                <Dropdown.Item className='dropdown-item-in-sidebar' href='#'> Outdoor Umbrella </Dropdown.Item>
-            </Dropdown.Menu>
-
-        </Dropdown>  
-
+      <Dropdown 
+        onMouseEnter={() => setOpenDropdown('parksFurniture')} 
+        onMouseLeave={() => setOpenDropdown(null)}
+        show={openDropdown === 'parksFurniture'}
+      >
+        <Dropdown.Toggle className="custom-sidebar-button">
+          Parks & Outdoor Furniture
+        </Dropdown.Toggle>
+        <Dropdown.Menu id='sidebar-dropdown-menu'>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Electric BBQ Pits</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Outdoor Furniture</Dropdown.Item>
+          <Dropdown.Item className='dropdown-item-in-sidebar' href='#'>Outdoor Umbrella</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>  
     </>
   );
 
   return (
     <div>
-
-      {/* Show the button only on small screens */}
+        {/* Show the button only on small screens */}
         {!isLargeScreen && (
             <Button onClick={handleShow} className="d-lg-none offcanvas-button">
-          Menu
+                Menu
             </Button>
         )}
 
-      {/* Offcanvas sidebar for small screens */}
+        {/* Offcanvas sidebar for small screens */}
         <Offcanvas show={show} onHide={handleClose}>
+
             <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Quick Links</Offcanvas.Title>
+                <Offcanvas.Title>Quick Links</Offcanvas.Title>
             </Offcanvas.Header>
 
-            <Offcanvas.Body>
-                {renderDropdowns()} {/* Render dropdowns */}
-            </Offcanvas.Body>
-            
+        <Offcanvas.Body>
+          {renderDropdowns()} {/* Render dropdowns */}
+        </Offcanvas.Body>
+
       </Offcanvas>
 
-        {/* Sidebar for large screens */}
-        {isLargeScreen && (
-            <div className="d-none d-lg-block sidebar">
-                {renderDropdowns()} {/* Render dropdowns */}
-            </div>
-        )}
-
+      {/* Sidebar for large screens */}
+      {isLargeScreen && (
+        <div className="d-none d-lg-block sidebar">
+          {renderDropdowns()} {/* Render dropdowns */}
+        </div>
+      )}
     </div>
   );
 };
