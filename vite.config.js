dfc669734ec23be09f553f7d0795af11c897fe88
Thamaru <thamarujalthotage1@gmail.com>
2024-11-07 08:38:42 +0530

    Base URL set to '/cwbsolutions.net/'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CWB-Solutions-Pte-Ltd/', // Add this line
+  base: '/cwbsolutions.net/', // Add this line
 });
 
 
