cbe8ba363db60d5ecc96d5eca7fcb8ce82c5559c
Thamaru <thamarujalthotage1@gmail.com>
2024-11-09 08:18:57 +0530

    Electric BBQ Pits page done

--- src/Site Pages/Products and Services/Electric BBQ Pits/Electric BBQ Pits.jsx
+++ src/Site Pages/Products and Services/Electric BBQ Pits/Electric BBQ Pits.jsx
@@ -1,9 +1,154 @@
+import { Carousel } from 'react-bootstrap';
 import './Electric BBQ Pits.css'
+import BBQWithSink from '../../Home Page/Images/u3fa8f55d403_original_600.jpeg'
+import cubeBBQ from '../../Home Page/Images/u23a88233395_original_600.jpeg'
+import dobuleSinkBBQ from '../../Home Page/Images/u149841a6401_original_600.jpeg'
+import electricBBQ from '../../Home Page/Images/Electric BBQ Pit.jpeg'
+import inBenchBBQ from '../../Home Page/Images/u370e2e26407_original_600.jpeg'
+import electricBBQDescription from '../../Home Page/Images/Electric BBQ Pit Description.jpeg'
 
 const ElectricBBQPits =() =>{
     return(
         <div className='electric-bbq-pits-container'>
-            <h1>Electric BBQ Pits</h1>
+            <h1>SMART ELECTRIC BBQ PIT RANGE</h1>
+
+            <Carousel id='carousel-in-electric-bbq-pits'>
+                <Carousel.Item interval={2500} >
+                        <img className='carousle-item' src={electricBBQ} alt="" />
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={2500} >
+                        <img className='carousle-item' src={cubeBBQ} alt="" />
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={2500} >
+                        <img className='carousle-item' src={BBQWithSink} alt="" />
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={2500} >
+                        <img className='carousle-item' src={dobuleSinkBBQ} alt="" />
+                    </Carousel.Item>
+
+            </Carousel>
+
+            <p>There’s nothing better than a barbecue to enliven a condo, park or public space and bring people together.</p>
+
+            <p>Park or Condo managers understand risk management is crucial and it’s not only the matter of avoiding accidents, but hygien as well. Dedicated to safety every electric BBQ Pit has patented safety features. It’s controls at the front of the cooktop, above the benchtop. This patented feature makes them easier not only see and use but also places them even further out of the reach of children. The modular system comes in one, two or three module combinations: electric or even a sink. Bonded dual-layer technology spreads the energy more evenly across every part of cooking surface. It distributes heat to all corners of the plate, even the side walls.</p>
+
+            <div className='electric-bbq-pits-applications-container'>
+                <h4>Applications</h4>
+
+                <ul>
+                    <li>Condominiums</li>
+                    <li>Parks</li>
+                    <li>Resorts</li>
+                    <li>Private Residences</li>
+                </ul>
+            </div>
+
+            <div className='electric-bbq-pits-features-container'>
+                <h4>Features</h4>
+
+                <ul>
+                    <li>Quick & Easy installation </li>
+                    <li>Minimal environmental impact </li>
+                    <li>Simple & Easy maintenance</li>
+                    <li>Modular system covers every option with numerous combinations of electric models or even a sink</li>
+                    <li>Bonded dual-layer technology </li>
+                    <li>Controls at the front of the cooktop, above the benchtop</li>
+                    <li>Audible signal to alert the user whenever there is a change in operating mode</li>
+                    <li> 6 seconds safety delay</li>
+                </ul>
+
+            </div>
+
+
+            <div className='electrict-bbq-types-container'>
+                <div><h2>INBENCH ELECTRIC BBQ PIT</h2></div>
+                <div><img src={inBenchBBQ} alt="" /></div>
+                <div>
+                The new Inbench BBQ pit gives you the ultimate barbecuing experience - you can cook twice as much with the efficient Smarter BBQ design that directs the heat where you need it. Even better. It cooks not stews your steaks! quality & durability. <br /><br />
+
+                Our Inbench Barbecue Unit is available in a 10amp (GXA-10) or 15amp (GXA-15) option Under and over voltage protection with automatic self-resetting. <br /><br />
+
+                Removable Inbench Unit for fire or flood prone areas Adjustable cooking temperature (175 - 275 degrees C) Adjustable cooking time (10 - 40min) 5 second delay child lock feature (can be turned off and on easily) Audible buzzer to alert when Barbeque is turning on and off Very easy and safe to clean Cooking hot plate made from 316 stainless steel ideal for Parks Sporting and recreational areas Tourist parks Resorts Multi Residential.
+                </div>
+            </div>
+
+            <div className='electrict-bbq-types-container'>
+                <div><h2>CUBE ELECTRIC BBQ PIT</h2></div>
+                <div><img src={cubeBBQ} alt="" /></div>
+                <div>
+                    <h4>We are NPARKS ELECTRICAL BBQ PIT 1st in Singapore @Sembawang Park Supplier with booking integration to AXS!</h4>
+
+
+                    <h5>ELECTRICAL BBQ PIT (Sembawang Park) </h5>
+
+                    <p> <b>1. What is an electrical BBQ pit? </b>
+                        <br />
+                        It is a BBQ pit that runs on electricity and is activated by a PIN.You do not need any charcoal for barbecuing. 
+                    </p>
+
+                    <p> <b>2. Where are the electrical BBQ pits located? </b>
+                        <br />
+                        They are currently located only at Sembawang Park, Pit 9 and 10. 3.  
+                    </p>
+
+                    <p> <b>3.Where can I make booking for electrical pits? </b>
+                        <br />
+                        The booking channels are the same as booking for normal BBQ pits. You may book for BBQ pit via AXS Stations, Online (NParks BBQ Pit Booking internet portal) and Phone Booking via AXS Hotline 6560 2727 (8am – 10pm daily). 
+                    </p>
+
+
+
+                    <p>Custom Booking Platform available through PC Standalone and Web & App integrations & Custom design <a href="https://www.axs.com.sg/help_NParksBBQFAQ.php">Click Here</a></p>
+
+                    <button>Read More</button>
+
+                </div>
+
+            </div>
+
+
+            <div className='electrict-bbq-types-container'>
+                <div><h2>Customised Electric BBQ Pit</h2></div>
+                <div>Imaage</div>
+                <div>
+                    <p>This innovative customized BBQ Pit is a patented safety technology which can complete your park or resting area</p>
+
+                    <p>Park or Condo managers understand risk management is crucial and it’s not only the matter of avoiding accidents, but hygiene as well. This customized BBQ Pit dedicated to safety. Every customized BBQ Pit has patented safety features. It’s controls at the front  of the cooktop, above the bench-top. This patented feature makes them easier not only see and use but also places them even further out of the reach of children.</p>
+
+                    <p>The modular system comes in one, two or three module combinations:  gas, electric or even a sink. It’s the only park BBQ with automatic temperature control on both gas and electric models.Bonded dual-layer technology spreads the energy more evenly across every part of cooking surface. It distributes heat to all corners of the plate, even the side walls. </p>
+                </div>
+
+                <h4>Applications</h4>
+
+                <ul>
+                    <li>Condominiums,</li>
+                    <li>Parks</li>
+                    <li>Resorts</li>
+                    <li>Private Residences</li>
+                </ul>
+
+
+                <div>
+                    <h4>Features</h4>
+
+                    <ul>
+                        <li>Quick & Easy installation </li>
+                        <li>Minimal environmental impact </li>
+                        <li>Simple & Easy maintenance</li>
+                        <li>Modular system covers every option with numerous combinations of electric models or even a sink</li>
+                        <li>Bonded dual-layer technology </li>
+                        <li>Controls at the front of the cooktop, above the benchtop</li>
+                        <li>Audible signal to alert the user whenever there is a change in operating mode</li>
+                        <li> 6 seconds safety delay</li>
+                    </ul>
+
+                </div>
+
+            </div>
+
         </div>
     )
 }
